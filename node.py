e8522fb8b5cb7821f3c3873412862f5c2ebc1df3
hennything00 <kok.gareth@gmail.com>
2021-11-28 17:27:37 +0100

    initial branch commit

--- node.py
+++ node.py
@@ -1,9 +1,16 @@
 
-# class Node:
+class Node:
 
-    # def __init__(self, __)
+    def __init__(self, id):
+        self.id = id
+        self.pred_id = None
+        self.succ_id = None
+        self.finger_table = {}
+        self.storage = {}
 
-    # def create(self):
+    def create(self):
+        self.pred_id = None
+        # self.succ_id = n
     
     # def join(self, node):
 
