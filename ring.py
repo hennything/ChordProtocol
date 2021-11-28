7ebed8bdc3d734c3d8d02beb88c92ebd3a6232a8
hennything00 <kok.gareth@gmail.com>
2021-11-28 17:37:53 +0100

    initial commit

--- ring.py
+++ ring.py
@@ -1,6 +1,32 @@
 
-# class Ring:
+class Ring:
 
-    # def __init__(self, __):
+    def __init__(self):
+        '''
+        node_dict: 
+        node_list: list of nodes in the chord ring (numerical order)
+        num_nodes: number of nodes in the chord ring 
+        '''
+        self.node_dict = {}
+        self.node_list = []
+        self.num_nodes = 0
+
+    def add_node(self, id):
+        '''
+        adds a node to the chord ring.
+        '''
+        pass
+
+    def remove_node(self, id):
+        '''
+        removes a node from the chord ring.
+        '''
+        pass
+
+
+
+
+
+
 
     
