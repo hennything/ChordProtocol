27d35007edc14034483fa1baca5b17c72c180071
hennything00 <kok.gareth@gmail.com>
2021-12-08 20:17:29 +0100

    Forgot to switch to a new branch

--- node.py
+++ node.py
@@ -1,15 +1,28 @@
+import socket, random, sys, hashlib, os
+from collections import OrderedDict
+
+MAX_BITS = 10        # 10-bit
+MAX_NODES = 2 ** MAX_BITS
+# Takes key string, uses SHA-1 hashing and returns a 10-bit (1024) compressed integer
+def getHash(key):
+    result = hashlib.sha1(key.encode())
+    return int(result.hexdigest(), 16) % MAX_NODES
 
 class Node:
 
-    def __init__(self, id):
+    def __init__(self, ip, port):
         '''
         storage:
         '''
-        self.id = id
-        self.pred_id = None
-        self.succ_id = None
-        self.finger_table = {}
-        self.storage = {}
+        self.ip = ip
+        self.port = port
+        self.id = getHash(self.ip + ":" + str(self.port))
+        self.finger_table = OrderedDict()
+        
+        self.pred = (ip, port)
+        self.pred_id = self.id
+        self.succ = (ip, port)
+        self.succ_id = self.id
 
     def create(self):
         '''
@@ -54,3 +67,12 @@ class Node:
 
         '''
         pass
+
+
+if __name__ == '__main__':
+
+    IP = "127.0.0.1"
+    PORT = 8080
+
+    node_1 = Node(IP, PORT)
+    print("Node ID: ", node_1.id)
